edf9f856b9a50a67a48bb53637d691a74593a250
Patrick Fung <patrick@makestuffdostuff.com>
2016-04-22 12:59:50 -0400

    enable submodules on git clone, and give the directory to nginx user

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# Clone OJS repository into install directory
 git node['ojs']['install_dir'] do
   repository node['ojs']['git_repo']
   revision node['ojs']['git_revision']
+  enable_submodules true
+  user node['nginx']['user']
+  group node['nginx']['group']
 end
